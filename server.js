1049f1c70018c8e72ace8eb82602aa41ea575bb2
Pablo Ortega <ortegapablo@gmail.com>
2019-08-31 10:59:52 +0200

    incluison de stubs y mocks

---
+++ server.js
@@ -0,0 +1,132 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var datastore = require("nedb");
+var cors = require('cors');
+var path = require('path');
+const NOTAS_APP_DIR = "/dist/notas-gui";
+
+
+var urlBase = "/api/v1";
+var filename = __dirname + "/notas.json";
+var notas = [{ "titulo": "Nota de prueba", "contenido": "recordar completar la colección de todos los metodos de la api en postman" }];
+var db = new datastore({
+    filename: filename,
+    autoload: true
+
+});
+
+
+
+var app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, NOTAS_APP_DIR)));
+app.get('/', function (req, res) {
+    res.sendFile(paht.join(__dirname, NOTAS_APP_DIR, '/index.html'));
+});
+
+
+
+app.get(urlBase + "/notas", (req, res) => {
+    db.find({}, (err, notas) => {
+        if (err) {
+            console.error("Error accediendo a la base de datos");
+            res.sendStatus(500);
+        } else {
+            res.send(notas.map((nota) => {
+                delete nota._id;
+                return nota;
+            }));
+        }
+    })
+});
+
+
+app.post(urlBase + "/notas", (req, res) => {
+    db.insert(req.body)
+    //notas.push(req.body);
+    // res.sendStatus(201);
+    res.status(200).send('Nota guardada')
+});
+
+app.put(urlBase + "/notas", (req, res) => {
+    res.status(405).send("¡no puedes actualizar todo a la vez!");
+});
+
+app.delete(urlBase + "/notas", (req, res) => {
+    db.remove({});
+    res.sendStatus(200);
+});
+
+
+app.get(urlBase + "/notas/:titulo", (req, res) => {
+    var titulo = req.params.titulo;
+    db.find({ "titulo": titulo }, (err, notas) => {
+        if (err) {
+            res.sendStatus(500);
+        } else {
+            if (notas.length == 0) {
+                res.status(404).send("No se ha encontrado la nota");
+            } else if (notas.length > 1) {
+                console.warn("Entrada duplicada...");
+                res.send(notas.map((nota) => {
+                    delete nota._id;
+                    return nota;
+                })[0]);
+            } else {
+                res.send(notas.map((nota) => {
+                    delete nota._id;
+                    return nota;
+
+
+                })[0]);
+            }
+        }
+    });
+
+});
+
+app.delete(urlBase + "/notas/:titulo", (req, res) => {
+    var titulo = req.params.titulo;
+    db.remove({ "titulo": titulo }, (err, eliminada) => {
+        if (err) {
+            console.error("Error accediendo a la base datos");
+            res.sendStatus(500);
+        } else {
+            if (eliminada > 1) {
+                console.warn("Entradas duplicada...");
+            } else if (eliminada == 0) {
+                res.status(404).send("No se ha encontrado la nota");
+            } else {
+                res.status(201).send("Nota eliminada correctamente");
+            }
+        }
+    });
+
+});
+
+app.put(urlBase + "/notas/:titulo", (req, res) => {
+    var titulo = req.params.titulo;
+    var cambioNota = req.body;
+    if (titulo != cambioNota.titulo) {
+        res.sendStatus(409);
+        return;
+    }
+    db.update({ "titulo": titulo }, cambioNota, (err, actulizado) => {
+        if (err) {
+            console.error("Error accediendo a la base datos");
+            res.sendStatus(500);
+        } else {
+            if (actulizado > 1) {
+                console.warn("Entradas duplicada...");
+            } else if (actulizado == 0) {
+                res.status(404).send("No se ha encontrado la nota");
+            } else {
+                res.status(201).send("Nota actualizada correctamente");
+            }
+        }
+    });
+});
+
+module.exports.app = app;
+module.exports.db = db;
--- test/server.test.js
+++ test/server.test.js
@@ -1,7 +1,10 @@
-
+var server = require('../server');
 var chai = require('chai');
-var expct = chai.expect;
+var chaiHttp = require('chai-http');
+var sinon = require('sinon');
+var expect = chai.expect;
 
+chai.use(chaiHttp);
 
 describe("Notas API", () => {
     it('hola mundo', (done) => {
@@ -9,7 +12,75 @@ describe("Notas API", () => {
         var y = 5;
         var resultado = x + y;
 
-        expct(resultado).to.equal(8);
+        expect(resultado).to.equal(8);
         done();
+    });
+
+    before((done) => {
+        var notas = [{ "titulo": "prueba 2", "contenido": "contenido de la tercera nota", "fecha": "22/08/2019" },
+        { "titulo": "prueba 3", "contenido": "contenido de la tercera nota", "fecha": "22/08/2019" }];
+        
+        var dbFindStub = sinon.stub(server.db, 'find');
+        dbFindStub.yields(null, notas);
+
+        done();
+        // server.db.remove({}, { multi: true }, () => {
+        //     server.db.insert(notas, () => {
+        //         done();
+        //     });
+        // });
+    });
+    describe("Get raiz /", () => {
+        it("should return HTML", done => {
+            chai
+                .request(server.app)
+                .get("/")
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res).to.be.html;
+                    done();
+                });
+        });
+    });
+
+    describe('Get /notas', () => {
+        it("should return all notas", done => {
+            chai
+                .request(server.app)
+                .get("/api/v1/notas")
+                // .query({ apikey: "test" })
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.an("array");
+                    expect(res.body).to.have.lengthOf(2);
+                    // proyectMock.verify();
+                    done();
+                });
+
+
+
+
+        });
+    });
+    describe('Post /notas', () => {
+        it('Should create a note.', done => {
+            var nota = { "titulo": "chaipost", "contenido": "contenido de la nota", "fecha": "22/08/2019" };
+            var dbMock = sinon.mock(server.db);
+            dbMock.expects('insert').withArgs(nota);
+            chai
+                .request(server.app)
+                .post("/api/v1/notas")
+                .send(nota)
+                // .query({ apikey: "test" })
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    dbMock.verify();
+
+
+                    
+                    // proyectMock.verify();
+                    done();
+                });
+        });
     });
 });
