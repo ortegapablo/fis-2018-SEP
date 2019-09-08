9974c2009be9187046bb3ca6580bd9bf5f2c18e6
Pablo Ortega <ortegapablo@gmail.com>
2019-09-08 23:17:21 +0200

    todos los metodos de la api autenticados

--- server.js
+++ server.js
@@ -79,85 +79,90 @@ app.post(urlBase + "/notas",
         });
     });
 
-app.put(urlBase + "/notas", (req, res) => {
-    res.status(405).send("¡no puedes actualizar todo a la vez!");
-});
-
-app.delete(urlBase + "/notas", (req, res) => {
-    Nota.deleteMany({}, (err) => {
-        if (err) {
-            console.error("Error accesing DB");
-            res.sendStatus(500);
-        }
+app.put(urlBase + "/notas", passport.authenticate('localapikey', { session: false }),
+    (req, res) => {
+        res.status(405).send("¡no puedes actualizar todo a la vez!");
     });
-    res.sendStatus(200);
-});
 
-
-app.get(urlBase + "/notas/:titulo", (req, res) => {
-    var titulo = req.params.titulo;
-    Nota.find({ "titulo": titulo }, (err, notas) => {
-        if (err) {
-            res.sendStatus(500);
-        } else {
-            if (notas.length == 0) {
-                res.status(404).send("No se ha encontrado la nota");
-            } else if (notas.length > 1) {
-                console.warn("Entrada duplicada...");
-                res.send(notas.map((nota) => {
-                    return nota.cleanup();
-                })[0]);
-            } else {
-                res.send(notas.map((nota) => {
-                    return nota.cleanup();
-                })[0]);
+app.delete(urlBase + "/notas", passport.authenticate('localapikey', { session: false }),
+    (req, res) => {
+        Nota.deleteMany({}, (err) => {
+            if (err) {
+                console.error("Error accesing DB");
+                res.sendStatus(500);
             }
-        }
+        });
+        res.sendStatus(200);
     });
 
-});
 
-app.delete(urlBase + "/notas/:titulo", (req, res) => {
-    var titulo = req.params.titulo;
-    Nota.deleteMany({ "titulo": titulo }, (err, eliminada) => {
-        if (err) {
-            console.error("Error accediendo a la base datos");
-            res.sendStatus(500);
-        } else {
-            if (eliminada.n > 1) {
-                console.warn("Entradas duplicada...");
-            } else if (eliminada.n == 0) {
-                res.status(404).send("No se ha encontrado la nota");
+app.get(urlBase + "/notas/:titulo", passport.authenticate('localapikey', { session: false }),
+    (req, res) => {
+        var titulo = req.params.titulo;
+        Nota.find({ "titulo": titulo }, (err, notas) => {
+            if (err) {
+                res.sendStatus(500);
             } else {
-                res.status(201).send("Nota eliminada correctamente");
+                if (notas.length == 0) {
+                    res.status(404).send("No se ha encontrado la nota");
+                } else if (notas.length > 1) {
+                    console.warn("Entrada duplicada...");
+                    res.send(notas.map((nota) => {
+                        return nota.cleanup();
+                    })[0]);
+                } else {
+                    res.send(notas.map((nota) => {
+                        return nota.cleanup();
+                    })[0]);
+                }
             }
-        }
+        });
+
     });
 
-});
-
-app.put(urlBase + "/notas/:titulo", (req, res) => {
-    var titulo = req.params.titulo;
-    var cambioNota = req.body;
-    if (titulo != cambioNota.titulo) {
-        res.sendStatus(409);
-        return;
-    }
-    Nota.replaceOne({ "titulo": titulo }, cambioNota, (err, actulizado) => {
-        if (err) {
-            console.error("Error accediendo a la base datos");
-            res.sendStatus(500);
-        } else {
-            if (actulizado.n > 1) {
-                console.warn("Entradas duplicada...");
-            } else if (actulizado.n == 0) {
-                res.status(404).send("No se ha encontrado la nota");
+app.delete(urlBase + "/notas/:titulo", passport.authenticate('localapikey', { session: false }),
+    (req, res) => {
+        var titulo = req.params.titulo;
+        Nota.deleteMany({ "titulo": titulo }, (err, eliminada) => {
+            if (err) {
+                console.error("Error accediendo a la base datos");
+                res.sendStatus(500);
             } else {
-                res.status(201).send("Nota actualizada correctamente");
+                if (eliminada.n > 1) {
+                    console.warn("Entradas duplicada...");
+                } else if (eliminada.n == 0) {
+                    res.status(404).send("No se ha encontrado la nota");
+                } else {
+                    res.status(201).send("Nota eliminada correctamente");
+                }
             }
-        }
+        });
+
     });
-});
+
+app.put(urlBase + "/notas/:titulo", passport.authenticate('localapikey', { session: false }),
+    (req, res) => {
+        var titulo = req.params.titulo;
+        var cambioNota = req.body;
+        if (titulo != cambioNota.titulo) {
+            res.sendStatus(409);
+            return;
+        }
+        Nota.replaceOne({ "titulo": titulo }, cambioNota, (err, actulizado) => {
+            if (err) {
+                console.error("Error accediendo a la base datos");
+                res.sendStatus(500);
+            } else {
+                if (actulizado.n > 1) {
+                    console.warn("Entradas duplicada...");
+                } else if (actulizado.n == 0) {
+                    res.status(404).send("No se ha encontrado la nota");
+                } else {
+                    res.status(201).send("Nota actualizada correctamente");
+                }
+            }
+        });
+    });
 
 module.exports.app = app;
 module.exports.db = db;
